5329fcbd4d0f978ed47c8445bde9cf51ac6fc40e
Michał Kołsut <michalkolsut@Air-Micha.home>
2024-02-20 20:55:00 +0100

    Add locators in my accout page

--- pages/MyAccountPage.ts
+++ pages/MyAccountPage.ts
@@ -2,4 +2,14 @@ import { Page } from "@playwright/test";
 
 export class MyAccountPage {
   constructor(private page: Page) {}
+
+  registerButton = this.page.getByRole("button", { name: "Register" });
+  loginTextbox = this.page.locator("#reg_email");
+  passwordTextbox = this.page.locator("#reg_password");
+
+  async corectSignIn(login: string, password: string): Promise<void> {
+    await this.loginTextbox.fill(login);
+    await this.passwordTextbox.fill(password);
+    this.registerButton.click();
+  }
 }
--- tests/signUp.spec.ts
+++ tests/signUp.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { LandingPage } from "../pages/landingPage";
+import { MyAccountPage } from "../pages/myAccountPage";
 
 test.describe("User sign in Generic shop", () => {
   test.beforeEach(async ({ page }) => {
@@ -9,11 +10,11 @@ test.describe("User sign in Generic shop", () => {
   test("User wants to create account using empty login and password textbox", async ({
     page,
   }) => {
-    //await page.getByRole("link", { name: "Account" }).click();
     const landingPage = new LandingPage(page);
     await landingPage.accountButton.click();
+    const myAccountPage = new MyAccountPage(page);
     // check if url is correct
-    await page.getByRole("button", { name: "Register" }).click();
+    myAccountPage.registerButton.click();
     await expect(page.getByText("Error: Please provide a valid ")).toHaveText(
       "Error: Please provide a valid email address."
     );
@@ -23,10 +24,12 @@ test.describe("User sign in Generic shop", () => {
     page,
   }) => {
     const landingPage = new LandingPage(page);
+    const myAccountPage = new MyAccountPage(page);
+    // check url
     await landingPage.accountButton.click();
-    //await page.getByRole("link", { name: "Account" }).click();
-    await page.locator("#reg_email").fill("wrongaddress");
-    await page.getByRole("button", { name: "Register" }).click();
+    await myAccountPage.loginTextbox.fill("das");
+    await myAccountPage.registerButton.click();
+    //await page.getByRole("button", { name: "Register" }).click();
     // add assertion for pop-up
   });
 
