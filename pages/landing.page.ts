a8b641964503a9de8f85b31e5396a06922e5e37d
Michał Kołsut <michalkolsut@Air-Micha.home>
2024-02-22 00:26:36 +0100

    Add move to my account method

--- pages/landing.page.ts
+++ pages/landing.page.ts
@@ -4,4 +4,10 @@ export class LandingPage {
   constructor(private page: Page) {}
 
   accountButton = this.page.getByRole("link", { name: "Account" });
+
+  moveToMyAcount = async () => {
+    await this.accountButton.waitFor();
+    await this.accountButton.click();
+    await this.page.waitForURL(/\/my-account/, { timeout: 3000 });
+  };
 }
