17d018461ddf51d116cc8a3c2af37786341aff07
Michał Kołsut <michalkolsut@Air-Micha.home>
2024-02-22 00:28:03 +0100

    Add sign and remember user and move to lost password method

--- pages/myaccount.page.ts
+++ pages/myaccount.page.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 
 export class MyAccountPage {
   constructor(private page: Page) {}
@@ -10,14 +10,35 @@ export class MyAccountPage {
   emailForLogin = this.page.locator("#username");
   passwordForLogin = this.page.locator("#password");
   rememberMeCheckbox = this.page.locator("#rememberme");
-  //lostPasswordLink = this.page.locator("");
+  lostPasswordLink = this.page.getByRole("link", {
+    name: "Lost your password?",
+  });
+
   loginButton = this.page.locator("#login");
 
-  errorMessage = this.page.locator(".woocommerce-error");
+  loginError = this.page.locator("aria-live='polite'");
 
-  async correctSignIn(login: string, password: string): Promise<void> {
+  passwordError = this.page.locator("*.woocommerce-password-strength");
+
+  signIn = async (login: string, password: string): Promise<void> => {
+    await this.emailForReg.waitFor();
     await this.emailForReg.fill(login);
+    await this.passwordForReg.waitFor();
     await this.passwordForReg.fill(password);
+    await this.registerButton.waitFor();
     await this.registerButton.click();
-  }
+  };
+
+  rememberUser = async () => {
+    await this.rememberMeCheckbox.waitFor();
+    expect(this.rememberMeCheckbox.isChecked).toBeFalsy();
+    await this.rememberMeCheckbox.click();
+    await expect(this.rememberMeCheckbox).toBeChecked();
+  };
+
+  moveToLostPassword = async () => {
+    //await this.lostPasswordLink.waitFor();
+    await this.lostPasswordLink.click();
+    await this.page.waitForURL(/\/lost-password/, { timeout: 3000 });
+  };
 }
