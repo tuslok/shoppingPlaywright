7964a08097b8d199e84f20584e7797c79b80b639
Michał Kołsut <michalkolsut@Air-Micha.home>
2024-02-23 00:13:43 +0100

    Add forget password test and edit last password page

---
+++ pages/lastpassword.page.ts
@@ -0,0 +1,17 @@
+import { Page } from "@playwright/test";
+
+export class LastPasswordPage {
+  constructor(private page: Page) {}
+
+  userLogin = this.page.locator("#user_login");
+  restartPasswordButton = this.page.getByRole("button", {
+    name: "Reset password",
+  });
+
+  retrievePassword = async (email: string): Promise<void> => {
+    await this.restartPasswordButton.waitFor();
+    await this.restartPasswordButton.click();
+    await this.userLogin.waitFor();
+    await this.userLogin.fill(email);
+  };
+}
--- tests/forgetPassword.spec.ts
+++ tests/forgetPassword.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { LandingPage } from "../pages/landing.page";
 import { MyAccountPage } from "../pages/myaccount.page";
+import { LastPasswordPage } from "../pages/lastpassword.page";
 
 test.describe("User sign in Generic shop", () => {
   test.beforeEach(async ({ page }) => {
@@ -10,7 +11,10 @@ test.describe("User sign in Generic shop", () => {
   test("User wants to retrieve password", async ({ page }) => {
     const landingPage = new LandingPage(page);
     const myAccountPage = new MyAccountPage(page);
+    const lastPasswordPage = new LastPasswordPage(page);
     await landingPage.moveToMyAcount();
     await myAccountPage.moveToLostPassword();
+    await lastPasswordPage.retrievePassword("");
+    await lastPasswordPage.retrievePassword("random@test.com");
   });
 });
