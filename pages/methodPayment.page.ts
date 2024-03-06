b1e0f0e0a90eba54beb95a0dc820027a901520a8
Michał Kołsut <michalkolsut@Air-Micha.home>
2024-03-06 13:04:42 +0100

    Add method payment for checkout test

---
+++ pages/methodPayment.page.ts
@@ -0,0 +1,30 @@
+import { Page } from "@playwright/test";
+
+export class MethodPayment {
+  constructor(private page: Page) {}
+
+  bankTransfer = this.page.locator("#payment_method_bacs");
+  checkPayment = this.page.locator("#payment_method_cheque");
+  cashOnDelivery = this.page.locator("#payment_method_cod");
+  paypalPayment = this.page.locator("#payment_method_paypal");
+
+  selectBankAccount = async () => {
+    await this.bankTransfer.waitFor();
+    await this.bankTransfer.click();
+  };
+
+  selectCheckPayment = async () => {
+    await this.checkPayment.waitFor();
+    await this.checkPayment.click();
+  };
+
+  selectCashOnDelivery = async () => {
+    await this.cashOnDelivery.waitFor();
+    await this.cashOnDelivery.click();
+  };
+
+  selectpaymentPayment = async () => {
+    await this.paypalPayment.waitFor();
+    await this.paypalPayment.click();
+  };
+}
