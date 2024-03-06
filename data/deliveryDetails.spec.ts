d34d7ab72bb6487d2682358cea63fa69bb056f31
Michał Kołsut <michalkolsut@Air-Micha.home>
2024-03-06 13:04:14 +0100

    Add delivery details for checkout test

---
+++ data/deliveryDetails.spec.ts
@@ -0,0 +1,10 @@
+export const deliveryDetails = {
+  firstName: "Marcus",
+  lastName: "O'Shea",
+  country: "Poland",
+  streetAddress: "Wojska Polskiego 45/23 m.7",
+  postalCode: "9313XO",
+  city: "Szczebrzeszyn Łódzki",
+  phone: "+00 443 432 623",
+  emailAddress: "simply@thebest.com",
+};
